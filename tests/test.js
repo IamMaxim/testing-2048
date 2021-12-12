4cf6d62118b2a2e34d1352f185433df30ffaf6f0
IamMaxim <g.maxim.stepanoff@gmail.com>
2021-12-12 16:44:18 +0300

    Fixed compareGrids

--- tests/test.js
+++ tests/test.js
@@ -33,29 +33,6 @@ describe('Unit tests', function () {
                     }), args[2]);
                 });
             });
-            it('should return false when out-of-bounds coordinate is passed (Blackbox BVA)', function () {
-                // Use Boundary Value Analysis.
-                // Since we have 4x4 grid in the game (this amount is not dynamic), we know for sure that suitable
-                // range is [0, 3]. We also don't know anything about internal logic, so this is a black-box test.
-                let values = [
-                    [0, 0, true],
-                    [0, -1, false],
-                    [-1, 0, false],
-                    [3, 3, true],
-                    [4, 3, false],
-                    [3, 4, false],
-                ];
-
-                let grid = new g.Grid(4);
-
-                values.forEach(function (args) {
-                    assert.equal(grid.withinBounds({
-                        x: args[0],
-                        y: args[1]
-                    }), args[2]);
-                });
-            });
-
         });
     });
     describe('Input', function () {
@@ -66,23 +43,18 @@ describe('Unit tests', function () {
             let gm = new gmm.GameManager(4, kim.KeyboardInputManager, ha.HTMLActuator, lsm.LocalStorageManager);
             let initialGrid = g.Grid.fromState(gm.grid.serialize());
 
-            // let modifiedGM = GameManager.fromState(gm.serialize());
-            // modifiedGM.move(0);
-            // let expectedGrid = modifiedGM.grid();
-
             let compareGrids = function (grid1, grid2) {
                 for (let i = 0; i < 4; i++)
-                    for (let j = 0; i < 4; j++) {
+                    for (let j = 0; j < 4; j++) {
                         if (grid1[i][j] !== grid2[i][j])
                             return false;
                     }
                 return true;
             };
 
-            let ev = new gm.KeyboardEvent('keydown', {keyCode: 39});
+            let ev = new KeyboardEvent('keydown', {keyCode: 39});
             dispatchEvent(ev);
 
-            // gm.move(0);
             let finalGrid = gm.grid;
 
             assert.ok(!compareGrids(initialGrid, finalGrid));
@@ -95,13 +67,9 @@ describe('Unit tests', function () {
             let gm = new gmm.GameManager(4, kim.KeyboardInputManager, ha.HTMLActuator, lsm.LocalStorageManager);
             let initialGrid = g.Grid.fromState(gm.grid.serialize());
 
-            // let modifiedGM = GameManager.fromState(gm.serialize());
-            // modifiedGM.move(0);
-            // let expectedGrid = modifiedGM.grid();
-
             let compareGrids = function (grid1, grid2) {
                 for (let i = 0; i < 4; i++)
-                    for (let j = 0; i < 4; j++) {
+                    for (let j = 0; j < 4; j++) {
                         if (grid1[i][j] !== grid2[i][j])
                             return false;
                     }
@@ -111,7 +79,6 @@ describe('Unit tests', function () {
             let ev = new KeyboardEvent('keydown', {keyCode: 1});
             dispatchEvent(ev);
 
-            // gm.move(0);
             let finalGrid = gm.grid;
 
             assert.ok(compareGrids(initialGrid, finalGrid));
@@ -137,7 +104,7 @@ describe('Unit tests', function () {
 
                 let compareGrids = function (grid1, grid2) {
                     for (let i = 0; i < 4; i++)
-                        for (let j = 0; i < 4; j++) {
+                        for (let j = 0; j < 4; j++) {
                             if (grid1[i][j] !== grid2[i][j])
                                 return false;
                         }
